dcabf8b69dd9c769b9fd7392ad730a39047b04c6
Ayush4NattyHatty <dhanrajaayush123@gmail.com>
2023-10-09 22:34:34 +0530

    Inception of react native

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {SafeAreaView, Text} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>This is my first React Native project in my personal laptop</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
